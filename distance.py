1a5a41b799a94acad1ee2fc4fffc5517188173bf
kosds <Kos-dan@mail.ru>
2017-09-14 19:58:16 +0300

    Dependence of distance from speed

---
+++ distance.py
@@ -0,0 +1,36 @@
+from math import *
+import numpy
+import matplotlib.pyplot as pyplot
+
+SPEED = 800
+HEIGHT = 1
+G = 9.8
+POINTS = 1000 
+DISTANCE_STD = 5
+
+
+def get_speed(distance):
+    return distance / sqrt(2 * HEIGHT / G)
+
+
+mean_distance = sqrt(2 * HEIGHT / G) * SPEED
+distances = numpy.random.normal(mean_distance, DISTANCE_STD, POINTS).tolist()
+speeds = []
+for d in distances:
+    speeds.append(get_speed(d))
+
+mean_speed = numpy.mean(speeds)
+std_speed = numpy.std(speeds)
+
+pyplot.plot(speeds, distances, 'bo')
+pyplot.plot([mean_speed, mean_speed], [min(distances), max(distances)],
+            color='red')
+pyplot.plot([mean_speed - std_speed, mean_speed - std_speed],
+            [min(distances), max(distances)], color='green')
+pyplot.plot([mean_speed + std_speed, mean_speed + std_speed],
+            [min(distances), max(distances)], color='green')
+pyplot.grid(True)
+pyplot.xlabel("speed")
+pyplot.ylabel("distance")
+pyplot.title("Mean: {0}, Std: {1}".format(mean_speed, std_speed))
+pyplot.show()
